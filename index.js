4758a4886a9ad84eaa0d1e04540fca0342470105
Huann Almeida <huannvictor@gmail.com>
2022-07-12 23:08:07 -0300

    closing menu when a link is clicked

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+//* applying toggle menu
+
 const nav = document.querySelector("nav");
 const toggle = document.querySelectorAll(".toggle");
 
 for (const element of toggle) {
   element.addEventListener("click", () => nav.classList.toggle("show"));
+}
+
+//* closing menu when a link is clicked
+
+const links = document.querySelectorAll("nav div.menu ul li a");
+
+for (const element of links) {
+  element.addEventListener("click", () => nav.classList.remove("show"));
 }
